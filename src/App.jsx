9c6f3fc74ce6971c00835abd63e21281ee752d72
Bikal <bikaladhikari5@gmail.com>
2024-05-30 19:11:28 +1000

    Landing page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Landing from "./pages/Landing";
 
 function App() {
-  return <>Hello World</>;
+  return (
+    <div>
+      <Landing />
+    </div>
+  );
 }
 
 export default App;

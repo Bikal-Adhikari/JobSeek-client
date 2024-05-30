f0afa54062d9158dfb35160ca4b689bf85901141
Bikal <bikaladhikari5@gmail.com>
2024-05-30 19:18:52 +1000

    Landing page is created

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,7 +2,28 @@ import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="JobSeek logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Find your perfect job easily. Create a profile, browse top
+            opportunities, and apply with one click. Start your career journey
+            today!
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="jobHunt" className="img main-img" />
+      </div>
+    </main>
+  );
 };
 
 export default Landing;

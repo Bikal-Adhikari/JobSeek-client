d5344a768903b6c8c6f3988c84f643bf349c0523
Bikal <bikaladhikari5@gmail.com>
2024-05-31 18:18:22 +1000

    Signup page is created will make the form into component later

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import { useState } from "react";
+import Logo from "../../components/Logo";
+import Wrapper from "./signup";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [values, setValues] = useState(initialState);
+
+  const handleOnChange = (e) => {
+    console.log(e.target);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper>
+      <form className="form" onSubmit={handleOnSubmit}>
+        <Logo />
+        <h3>Sign-Up</h3>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            value={values.name}
+            name="name"
+            onChange={handleOnChange}
+            className="form-input"
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default SignUp;

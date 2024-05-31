cd44610c92a4bf1c36ae3cac3ec78c6e61054151
Bikal <bikaladhikari5@gmail.com>
2024-05-31 17:39:28 +1000

    SignIn page is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import SignIn from "./pages/SignIn/SignIn";
 import Landing from "./pages/landingPage/Landing";
 
 function App() {
   return (
     <div>
       <Landing />
+      <SignIn />
     </div>
   );
 }

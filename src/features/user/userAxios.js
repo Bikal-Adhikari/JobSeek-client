04068ba42a60c5f9937cd7d62d4d90291a9534f8
Bikal <bikaladhikari5@gmail.com>
2024-06-01 21:29:12 +1000

    User action and user axios are created

---
+++ src/features/user/userAxios.js
@@ -0,0 +1,28 @@
+import { apiProcessor } from "../../utils/helpers/axiosHelpers";
+const serverURL = import.meta.env.VITE_APP_ROOT_SERVER;
+const userEP = serverURL + "/api/v1/users";
+
+export const loginUser = async (obj) => {
+  const axiosObj = {
+    method: "post",
+    url: userEP + "/login",
+    data: obj,
+  };
+  return apiProcessor(axiosObj);
+};
+export const postNewUser = async (obj) => {
+  const axiosObj = {
+    method: "post",
+    url: userEP,
+    data: obj,
+  };
+  return apiProcessor(axiosObj);
+};
+export const fetchUserInfo = async () => {
+  const axiosObj = {
+    method: "get",
+    url: userEP,
+    isPrivate: true,
+  };
+  return apiProcessor(axiosObj);
+};

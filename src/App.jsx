e98d2d89910ac46ce94fac0a232b9383a6e612cf
Bikal <bikaladhikari5@gmail.com>
2024-06-01 20:50:25 +1000

    Signup page is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
-import SignIn from "./pages/SignIn/SignIn";
-import SignUp from "./pages/SignUp/SignUp";
+import SignIn from "./pages/SignIn/SignIn.jsx";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Landing from "./pages/landingPage/Landing";
+import { ToastContainer } from "react-toastify";
+import SignUp from "./pages/SignUp/SignUp.jsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/landing" element={<Landing />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="*" element={<ErrorPage />} />
-    </Routes>
+    <div>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <ToastContainer />
+    </div>
   );
 }
 
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,46 +1,91 @@
 import { useState } from "react";
 import Logo from "../../components/Logo";
 import Wrapper from "./signup";
-
-const initialState = {
-  name: "",
-  email: "",
-  password: "",
-  isMember: true,
-};
+import CustomInput from "../../components/CustomInput";
+import { toast } from "react-toastify";
 
 const SignUp = () => {
-  const [values, setValues] = useState(initialState);
+  const [form, setForm] = useState({});
 
   const handleOnChange = (e) => {
-    console.log(e.target);
+    const { name, value } = e.target;
+    console.log(name, value);
+    setForm({
+      ...form,
+      [name]: value,
+    });
   };
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target);
+    const { confirmPassword, ...rest } = form;
+
+    if (confirmPassword !== rest.password) {
+      return toast.error("password do not match");
+    }
   };
 
+  const inputs = [
+    {
+      label: "First Name",
+      name: "fName",
+      type: "text",
+      required: true,
+      placeholder: "Write your first Name",
+    },
+    {
+      label: "Last Name",
+      name: "lName",
+      type: "text",
+      required: true,
+      placeholder: "Write your Last Name",
+    },
+    {
+      label: "Phone",
+      name: "phone",
+      type: "number",
+      required: false,
+      placeholder: "0451266661",
+    },
+    {
+      label: "Email",
+      name: "email",
+      type: "email",
+      required: true,
+      placeholder: "email@email.com",
+    },
+    {
+      label: "Password",
+      name: "password",
+      type: "password",
+      required: true,
+      placeholder: "**********",
+    },
+    {
+      label: "Confirm Password",
+      name: "confirmPassword",
+      type: "password",
+      required: true,
+      placeholder: "**********",
+    },
+  ];
   return (
     <Wrapper>
       <form className="form" onSubmit={handleOnSubmit}>
         <Logo />
         <h3>Sign-Up</h3>
-        <div className="form-row">
-          <label htmlFor="name" className="form-label">
-            name
-          </label>
-          <input
-            type="text"
-            value={values.name}
-            name="name"
-            onChange={handleOnChange}
-            className="form-input"
-          />
-        </div>
+        {inputs.map((input, index) => (
+          <CustomInput key={index} {...input} onChange={handleOnChange} />
+        ))}
         <button type="submit" className="btn btn-block">
           submit
         </button>
+        <p>
+          Already have an account ?{"  "}
+          <a href="/sign-in" className="member-btn">
+            Login Now
+          </a>
+        </p>
       </form>
     </Wrapper>
   );

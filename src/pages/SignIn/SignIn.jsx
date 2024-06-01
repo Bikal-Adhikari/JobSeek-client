914cf1985a3acbb2a893f5462d41eb832290e697
Bikal <bikaladhikari5@gmail.com>
2024-06-01 21:42:43 +1000

    using dispacher and selector to verify the user and send him to dashboard

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,14 +1,23 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import Wrapper from "./signIn";
 import Logo from "../../components/Logo";
 import CustomInput from "../../components/CustomInput";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "../../features/user/userAxios";
+import { getUserObj } from "../../features/user/userAction";
 
 const SignIn = () => {
+  const dispatch = useDispatch();
   const emailRef = useRef("");
   const passRef = useRef("");
   const navigate = useNavigate();
+
+  const { user } = useSelector((state) => state.userInfo);
+  useEffect(() => {
+    user?._id && navigate("/");
+  }, [user?._id, navigate]);
 
   const handleOnSubmit = async (e) => {
     e.preventDefault();
@@ -17,6 +26,15 @@ const SignIn = () => {
     console.log(email, password);
     if (!email || !password) {
       return toast.error("Both field must be filled");
+    }
+    const { status, message, tokens } = await loginUser({ email, password });
+    toast[status](message);
+    //store token in the sessions
+    sessionStorage.setItem("accessJWT", tokens.accessJWT);
+    localStorage.setItem("refreshJWT", tokens.refreshJWT);
+
+    if (status === "success") {
+      dispatch(getUserObj());
     }
   };
 

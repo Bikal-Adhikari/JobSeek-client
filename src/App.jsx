fcbca3dd3b612f917ec8544b2dbfca81cf50d728
Bikal <bikaladhikari5@gmail.com>
2024-06-06 19:44:51 +1000

    Routing each pages

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ import SignUp from "./pages/SignUp/SignUp.jsx";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { autoLogin } from "./features/user/userAction.js";
+import Stats from "./pages/dashboard/Stats.jsx";
+import AllJobs from "./pages/dashboard/AllJobs.jsx";
+import AddJobs from "./pages/dashboard/AddJobs.jsx";
+import Profile from "./pages/dashboard/Profile.jsx";
 
 function App() {
   const dispatch = useDispatch();
@@ -18,7 +22,12 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Dashboard />}>
+          <Route index element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<AddJobs />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
         <Route path="/landing" element={<Landing />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />

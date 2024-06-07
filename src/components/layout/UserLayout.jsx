49c38bd06b739e82ca3d2c07f8637483537a9d6e
Bikal <bikaladhikari5@gmail.com>
2024-06-07 19:10:40 +1000

    Dashboard landing layout is created but still needs to style it

--- src/components/layout/UserLayout.jsx
+++ src/components/layout/UserLayout.jsx
@@ -3,26 +3,37 @@ import { useSelector } from "react-redux";
 import { AuthRoute } from "../auth/AuthRoute";
 import Navbar from "../Navbar";
 import { Footer } from "../Footer";
+import { Outlet } from "react-router-dom";
+import Wrapper from "./userlayout";
+import SmallSidebar from "../SmallSidebar";
+import BigSidebar from "../BigSidebar";
 
 export const UserLayout = ({ children, pageTitle }) => {
   const { user } = useSelector((state) => state.userInfo);
   return (
     <AuthRoute>
-      <Navbar />
-      <div className="p-3">
-        <div>Welcome Back</div>
-        <h3>{user.fName + " " + user.lName}</h3>
-      </div>
-      {/* <UserSidebar /> */}
-      {/* </Col> */}
-      {/* <Col> */}
-      <div className="p-2">{pageTitle}</div>
-      <hr />
-      <main className="main mb-3">{children}</main>
-      {/* </Col> */}
-      {/* </Row> */}
-      {/* </Container> */}
-      <Footer />
+      <Wrapper>
+        <main className="dashboard">
+          <SmallSidebar />
+          <BigSidebar />
+          <div>
+            <Navbar />
+          </div>
+          <div className="dashboard-page">
+            <Outlet />
+          </div>
+        </main>
+        <div className="p-3">
+          <div>Welcome Back</div>
+          <h3>{user.fName + " " + user.lName}</h3>
+        </div>
+
+        <div className="p-2">{pageTitle}</div>
+        <hr />
+        <main className="main mb-3">{children}</main>
+
+        <Footer />
+      </Wrapper>
     </AuthRoute>
   );
 };

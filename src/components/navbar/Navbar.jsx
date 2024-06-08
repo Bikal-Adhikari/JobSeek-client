61681c10ed717dfe62d634ec5e5b6cb5cd796997
Bikal <bikaladhikari5@gmail.com>
2024-06-08 21:51:09 +1000

    dropdown button to show logOut is working

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,10 +3,11 @@ import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import Logo from "../Logo";
 import { toggleSidebar } from "../../features/user/userSlice";
+import { useState } from "react";
 
 const Navbar = ({ pageTitle }) => {
   const { user } = useSelector((state) => state.userInfo);
-
+  const [showLogOut, setShowLogOut] = useState(false);
   const dispatch = useDispatch();
   const toggle = () => {
     dispatch(toggleSidebar());
@@ -25,13 +26,13 @@ const Navbar = ({ pageTitle }) => {
           <button
             type="button"
             className="btn"
-            onClick={() => console.log("toggle logout dropdown")}
+            onClick={() => setShowLogOut(!showLogOut)}
           >
             <FaUserCircle />
             {user?.fName}
             <FaCaretDown />
           </button>
-          <div className="dropdown show-dropdown">
+          <div className={showLogOut ? "dropdown show-dropdown" : "dropdown"}>
             <button
               type="button"
               className="dropdown-btn"

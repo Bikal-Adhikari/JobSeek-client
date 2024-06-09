7dac8572cd07454ffd499645765d84dc126fa1f4
Bikal <bikaladhikari5@gmail.com>
2024-06-09 18:26:27 +1000

    Profile custom form is created

--- src/pages/dashboard/Profile.jsx
+++ src/pages/dashboard/Profile.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import Wrapper from "./DashboardFormPage";
+import CustomInput from "../../components/CustomInput";
 
 const Profile = () => {
   const { user } = useSelector((state) => state.userInfo);
@@ -14,9 +15,9 @@ const Profile = () => {
   });
   const handleSubmit = (e) => {
     e.preventDefault();
-    const { name, email, lastName, location } = userData;
+    const { fName, email, lName, location } = userData;
 
-    if (!name || !email || !lastName || !location) {
+    if (!fName || !email || !lName || !location) {
       toast.error("Please Fill Out All Fields");
       return;
     }
@@ -26,8 +27,54 @@ const Profile = () => {
     const value = e.target.value;
     setUserData({ ...userData, [name]: value });
   };
+  const inputs = [
+    {
+      label: "First Name",
+      name: "fName",
+      type: "text",
+      required: true,
+      placeholder: "Write your first Name",
+    },
+    {
+      label: "Last Name",
+      name: "lName",
+      type: "text",
+      required: true,
+      placeholder: "Write your Last Name",
+    },
+    {
+      label: "Email",
+      name: "email",
+      type: "email",
+      required: true,
+      placeholder: "email@email.com",
+    },
+    {
+      label: "Location",
+      name: "location",
+      type: "text",
+      required: true,
+      placeholder: "Write your City name",
+    },
+  ];
 
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <form className="form" onSubmit={handleSubmit}>
+        <h3>profile</h3>
+
+        <div className="form-center">
+          {inputs.map((input, i) => (
+            <CustomInput key={i} {...input} onChange={handleChange} />
+          ))}
+
+          <button className="btn btn-block" type="submit" disabled={isLoading}>
+            {isLoading ? "Please Wait..." : "save changes"}
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Profile;
